9b2ff155d964ba6ec29e7f1ae76fe0edeffa88ed
Marco Morales <marco.bbcm@gmail.com>
2023-11-13 08:26:16 -0800

    Adds Chakra UI base dependencies and UI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import {
+  ChakraBaseProvider,
+  extendTheme,
+  Card,
+  Box,
+  Container,
+  Heading,
+} from '@chakra-ui/react'
+
+const theme = extendTheme({
+  components: {},
+})
 
 const App: React.FunctionComponent = () => {
   return (
-    <div>
-      <h1>Token Generator</h1>
-    </div>
+    <ChakraBaseProvider theme={theme}>
+      <Container maxWidth={1600} p='64px'>
+        This is a Container
+        <Box m={2} p={2}>
+          This is a box
+          <Heading as='h1'>Token Generator</Heading>
+          <Card p={4}>This is a card</Card>
+        </Box>
+      </Container>
+    </ChakraBaseProvider>
   )
 }
 
