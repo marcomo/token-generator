70473dce7acc9fdd301e3e5b229c24cac0681626
Marco Morales <marco.bbcm@gmail.com>
2024-02-24 13:32:31 -0800

    Adds custom Chakra theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,11 +6,31 @@ import {
   Box,
   Container,
   Heading,
+  ChakraTheme,
 } from '@chakra-ui/react'
 
-const theme = extendTheme({
-  components: {},
-})
+const customTheme: Partial<ChakraTheme> = {
+  components: {
+    Heading: {
+      variants: {
+        h1: {
+          fontSize: '4xl',
+        },
+        h2: {
+          fontSize: '3xl',
+        },
+        h3: {
+          fontSize: '2xl',
+        },
+        h4: {
+          fontSize: 'xl',
+        },
+      },
+    },
+  },
+}
+
+const theme = extendTheme(customTheme)
 
 const App: React.FunctionComponent = () => {
   return (
