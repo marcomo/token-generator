bf675664c88d578572cfc8225072e5a1ce2355c3
Marco Morales <marco.bbcm@gmail.com>
2024-02-24 13:20:36 -0800

    Adds typography back to demo node script
    Adds previously remove typographic scale demos. Adds tsconfig specific to node script build.

--- src/demo/test.ts
+++ src/demo/test.ts
@@ -1,3 +1,35 @@
+import { TypeScaleOptions } from '../../src/types.js'
+import createTypescale from '../utils/createTypeScale.js'
 import mdfyStyleDictionary from '../sdconfig'
 
+export const typescaleDemo1 = () => {
+  const options1: TypeScaleOptions = {
+    base: 24,
+    maxSize: 72,
+    minSize: 16,
+    factor: 1.125,
+    roundBeforeBy: 4,
+    roundAfterBy: 8,
+  }
+  console.log('Test 1:')
+  console.log('TypeScale Options:', options1)
+  console.log('TypeScale Output:', createTypescale(options1))
+}
+
+export const typescaleDemo2 = () => {
+  const options2: TypeScaleOptions = {
+    base: 16,
+    factor: 1.125,
+    minSize: 12,
+    maxSize: 96,
+    roundBeforeBy: 4,
+    roundAfterBy: 4,
+  }
+  console.log('\nTest 2:')
+  console.log('TypeScale Options:', options2)
+  console.log('TypeScale Output:', createTypescale(options2))
+}
+
+typescaleDemo1()
+typescaleDemo2()
 mdfyStyleDictionary.buildAllPlatforms()
